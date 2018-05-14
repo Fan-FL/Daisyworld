f39aaeb1710b90842af417fc4007f2a8019e82f6
ANXIshow <31022589+ANXIshow@users.noreply.github.com>
2018-05-14 11:22:28 +1000

    Update Params.java

--- src/Params.java
+++ src/Params.java
@@ -1,12 +1,25 @@
+/**
+ * The class representing the initial parameters in the simulation
+ */
 public class Params {
+    // The simulation not stop
     public static boolean useMaxTick = false;
+    // The initial tick in the simulation
     public static int maxTick = 0;
+    // The default percentage of white daisy
     public static float startPctWhites = 0.2f;
+    // The default percentage of black daisy
     public static float startPctBlacks = 0.2f;
+    // The default albedo of white daisy
     public static float albedoOfWhites = 0.75f;
+    // The default albedo of black daisy
     public static float albedoOfBlacks = 0.25f;
+    // The default albedo of surface
     public static float albedoOfSurface = 0.4f;
+    // The default value of solar luminosity
     public static float solarLuminosity = 1.0f;
+    // 
     public static float diffusePct = 0.5f;
+    // The max age of daisy
     public static int maxAge = 25;
 }
