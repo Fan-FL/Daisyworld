0f70fd4e09a1df89c1d05d39ae2ec7f0654755e8
ANXIshow <31022589+ANXIshow@users.noreply.github.com>
2018-05-14 11:11:14 +1000

    Update Daisy.java

--- src/Daisy.java
+++ src/Daisy.java
@@ -1,5 +1,6 @@
-/* The daisy represents 
-*/
+/**
+ * The class representing a daisy's state
+ */
 public class Daisy{
     // this daisy's origin albedo
     protected float albedo = 0.5f;
@@ -10,10 +11,11 @@ public class Daisy{
     // the default species is black daisy
     protected Species species = Species.BLACK;
     
+    // @return whether the daisy could sprout
     public boolean isSprout() {
         return sprout;
     }
-
+    // mark whether the daisy could sprout
     public void setSprout(boolean sprout) {
         this.sprout = sprout;
     }
@@ -21,27 +23,27 @@ public class Daisy{
     public enum Species {
         BLACK, WHITE
     }
-
+    // @return the species of this daisy
     public Species getSpecies() {
         return species;
     }
-
+    // mark this daisy as black or white
     public void setSpecies(Species species) {
         this.species = species;
     }
-
+    // @return the albedo of this daisy
     public float getAlbedo() {
         return albedo;
     }
-
+    // mark this daisy's albedo 
     public void setAlbedo(float albedo) {
         this.albedo = albedo;
     }
-
+    // @return the age of this daisy
     public int getAge() {
         return age;
     }
-
+    // mark this daisy's age 
     public void setAge(int age) {
         this.age = age;
     }
