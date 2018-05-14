4748f2f5e9d1e2f4c2f26b3fa540ccf0bb86999a
ANXIshow <31022589+ANXIshow@users.noreply.github.com>
2018-05-14 10:59:05 +1000

    Update Daisy.java

--- src/Daisy.java
+++ src/Daisy.java
@@ -1,9 +1,15 @@
+/* The daisy represents 
+*/
 public class Daisy{
+    // this daisy's origin albedo
     protected float albedo = 0.5f;
+    // this daisy's initial age
     protected int age = 0;
+    // true if the daisy could sprout
     boolean sprout = true;
+    // the default species is black daisy
     protected Species species = Species.BLACK;
-
+    
     public boolean isSprout() {
         return sprout;
     }
@@ -11,7 +17,7 @@ public class Daisy{
     public void setSprout(boolean sprout) {
         this.sprout = sprout;
     }
-
+    // there are two types of daisy, black daisy and white daisy
     public enum Species {
         BLACK, WHITE
     }
